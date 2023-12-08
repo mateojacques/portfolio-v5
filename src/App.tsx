dabcdb25237ab0fcf0ae65e7a650cff149f3c2e7
Mateo Jacques <mateo.jacques@avenida.com>
2023-12-08 15:49:09 -0300

    master | Create two columns layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <main className="main">
+      <section className="left">
+        <div className="content">
+          <h1 className="f-s-48 m-b-10">Mateo Jacques</h1>
+          <h2 className="f-s-24 m-b-20">Fullstack Developer</h2>
+          <p className="f-s-16 m-b-60">
+            Delivering creative solutions to modern problems.
+          </p>
+          <nav className="navigation">
+            <a href="#">About</a>
+            <a href="#">Experience</a>
+            <a href="#">Projects</a>
+          </nav>
+        </div>
+      </section>
+      <section className="right"></section>
+    </main>
+  );
 }
 
 export default App;
